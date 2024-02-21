82e4cb138bbdbd9785fa82dc7c4984ea5d9d98c6
小煜 <ow666faka@qq.com>
2024-02-21 22:20:58 +0800

    Add bevy_editor_pls and bevy_atmosphere dependencies

--- examples/load_model.rs
+++ examples/load_model.rs
@@ -1,15 +1,15 @@
 //! Loads and renders a glTF file as a scene.
-
 use bevy::{pbr::DirectionalLightShadowMap, prelude::*};
-use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use bevy_atmosphere::prelude::*;
+use bevy_editor_pls::prelude::*;
 use bevy_npr::toon::{ToonBundle, ToonMaterial, ToonShaderPlugin};
 use std::f32::consts::{FRAC_PI_4, PI};
 
 fn main() {
     App::new()
         .insert_resource(DirectionalLightShadowMap { size: 4096 })
-        .add_plugins((DefaultPlugins, ToonShaderPlugin))
-        .add_plugins(WorldInspectorPlugin::new())
+        .add_plugins((DefaultPlugins, ToonShaderPlugin, AtmospherePlugin))
+        .add_plugins(EditorPlugin::default())
         .add_systems(PreStartup, setup)
         .add_systems(PreUpdate, update_materials)
         .add_systems(Update, animate_light_direction)
@@ -21,16 +21,20 @@ struct GLTFScene;
 
 fn setup(
     mut commands: Commands,
-    mut _standard_materials: ResMut<Assets<StandardMaterial>>,
+    mut standard_materials: ResMut<Assets<StandardMaterial>>,
     mut toon_materials: ResMut<Assets<ToonMaterial>>,
     mut meshes: ResMut<Assets<Mesh>>,
     asset_server: Res<AssetServer>,
 ) {
     info!("init");
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(3.0, 4.0, 2.0).looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::Y),
-        ..default()
-    });
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(3.0, 4.0, 2.0)
+                .looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::Y),
+            ..default()
+        },
+        AtmosphereCamera::default(),
+    ));
 
     commands.spawn(DirectionalLightBundle {
         directional_light: DirectionalLight {
@@ -47,30 +51,34 @@ fn setup(
         })
         .insert(GLTFScene);
 
-    commands.spawn(ToonBundle {
-        mesh: meshes.add(shape::Plane::from_size(50.0).into()),
-        material: toon_materials.add(Color::SILVER.into()),
-        ..default()
-    });
+    commands.spawn((
+        PbrBundle {
+            mesh: meshes.add(shape::Plane::from_size(50.0).into()),
+            material: standard_materials.add(Color::SILVER.into()),
+            ..default()
+        },
+        toon_materials.add(Color::SILVER.into()),
+    ));
 }
 
 fn update_materials(
     mut commands: Commands,
     mut materials: ResMut<Assets<ToonMaterial>>,
-    spheres: Query<(Entity, &Handle<StandardMaterial>, &Name)>,
+    spheres: Query<(Entity, &Handle<StandardMaterial>)>,
 ) {
     for sphere in spheres.iter() {
-        let (entity, material, name) = sphere;
-        info!(
-            "update_materials, entity: {:?}, material: {:?}, name: {:?}",
-            entity, material, name
-        );
-        commands
-            .entity(sphere.0)
-            .remove::<Handle<StandardMaterial>>();
-        commands
-            .entity(sphere.0)
-            .insert(materials.add(Color::SILVER.into()));
+        let (entity, material) = sphere;
+        match material.path() {
+            Some(path) => {
+                if path.to_string().contains("models/tuzi.glb#Material0") {
+                    commands.entity(entity).remove::<Handle<StandardMaterial>>();
+                    commands
+                        .entity(entity)
+                        .insert(materials.add(Color::SILVER.into()));
+                }
+            }
+            None => {}
+        }
     }
 }
 
